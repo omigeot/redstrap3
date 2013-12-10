4943cd70bf89e7a7d9bb588a4a813c6f458091c9
Olivier Migeot <olivier+github@migeot.org>
2013-12-10 13:11:11 +0100

    Fixes following the inclusion of Bootstrap in Red's core libraries

--- php/theme.php
+++ php/theme.php
@@ -8,17 +8,17 @@ function redstrap3_init(&$a) {
 	head_add_js('jquery.js');
 	head_add_js('bootstrap.min.js');
 	// Get the UID of the channel owner
-        $uid =  $a->profile_uid; 
-	if ($uid == 0) {
-		$uid = get_theme_uid();
-	}
-
-        if($uid)
-            load_pconfig($uid,'redstrap3');
-        $schema = get_pconfig($uid,'redstrap3','schema');
-	if (!$schema)
-		$schema = 'bootstrap.css';
-	head_add_css($schema);
+//        $uid =  $a->profile_uid; 
+//	if ($uid == 0) {
+//		$uid = get_theme_uid();
+//	}
+//
+//        if($uid)
+//            load_pconfig($uid,'redstrap3');
+//        $schema = get_pconfig($uid,'redstrap3','schema');
+//	if (!$schema)
+//		$schema = 'bootstrap.css';
+//	head_add_css($schema);
 	head_add_js('dna.min.js');
 	head_add_js('redstrap3.js');
 }
--- php/theme_init.php
+++ php/theme_init.php
@@ -2,8 +2,22 @@
 
 head_add_css('library/font_awesome/css/font-awesome.min.css');
 
+require_once('view/php/theme_init.php');
+
+// Get the UID of the channel owner
+$uid =  $a->profile_uid;
+if ($uid == 0) {
+	$uid = get_theme_uid();
+}
+
+if($uid)
+	load_pconfig($uid,'redstrap3');
+$schema = get_pconfig($uid,'redstrap3','schema');
+if (!$schema)
+	$schema = 'bootstrap.css';
+head_add_css($schema);
 head_add_css('style-large.css','screen and (min-width: 1360px)');
 head_add_css('style-medium.css','screen and (max-width: 1359px)');
 
 
-require_once('view/php/theme_init.php');
+
